9d56fd0090225a4e4de9a49861aa40b30498a64d
Srinivasan S <srinivasansureshmech@gmail.com>
2023-07-27 15:17:27 +0530

    hi

---
+++ index.js
@@ -0,0 +1,10 @@
+ const windowwidth = window.innerWidth;
+ const windowHeight = window.innerHeight;
+ window.scrollBy(0, 150);
+ console.log("Window width: ", windowWidth);
+ console.log("Window height: ", windowHeight);
+
+window.open("https://www.1mg.com/", "_blank");
+
+// window.close();
+console.log("Current URL: ", window.location.href);
